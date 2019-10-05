4dbe8d7a7cb590170ab1d22db761411ffcbebaa4
19042935 <630455664@qq.com>
2019-10-05 19:37:49 +0800

    首页推荐模块、流行模块完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,76 +6,98 @@ import {
 
 Page({
 
-      /**
-       * 页面的初始数据
-       */
-      data: {
-        banners: [{
-            image: "../../images/home/ic_banner_1.jpg"
-          },
-          {
-            image: "../../images/home/ic_banner_2.jpg"
-          },
-          {
-            image: "../../images/home/ic_banner_3.jpg"
-          }]},
-          /**
-           * 生命周期函数--监听页面加载
-           */
-          onLoad: function(options) {
-            //请求轮播图
-            // getBannerList().then(res => {
-            //   console.log
-            // })
-          },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners: [{
+        image: "/images/home/ic_banner_1.jpg"
+      },
+      {
+        image: "/images/home/ic_banner_2.jpg"
+      },
+      {
+        image: "/images/home/ic_banner_3.jpg"
+      }
+    ],
+    recommends: [{
+      "image": "/images/home/ic_classify_tip.png",
+      "tittle": "分类指南"
+    }, {
+        "image": "/images/home/ic_invite_gift.png",
+      "tittle": "邀请有礼"
+    }, {
+        "image": "/images/home/ic_integral_mall.png",
+      "tittle": "积分商城"
+    }, {
+        "image": "/images/home/ic_sign_integral.png",
+      "tittle": "签到积分"
+      }]
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    //请求轮播图
+    // getBannerList().then(res => {
+    //   console.log
+    // })
+  },
 
-          /**
-           * 生命周期函数--监听页面初次渲染完成
-           */
-          onReady: function() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-          },
+  },
 
-          /**
-           * 生命周期函数--监听页面显示
-           */
-          onShow: function() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-          },
+  },
 
-          /**
-           * 生命周期函数--监听页面隐藏
-           */
-          onHide: function() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-          },
+  },
 
-          /**
-           * 生命周期函数--监听页面卸载
-           */
-          onUnload: function() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-          },
+  },
 
-          /**
-           * 页面相关事件处理函数--监听用户下拉动作
-           */
-          onPullDownRefresh: function() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-          },
+  },
 
-          /**
-           * 页面上拉触底事件的处理函数
-           */
-          onReachBottom: function() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-          },
+  },
 
-          /**
-           * 用户点击右上角分享
-           */
-          onShareAppMessage: function() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
-          }
+  },
 
-        })
+  // 流行模块点击事件
+  onPopularClick:function(){
+    wx.navigateTo({
+      url: './collectToHome/collectToHome',
+    })
+  }
+
+})
