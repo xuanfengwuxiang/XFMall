0b314a95020bb784b3ee14b6ff990bec70433a54
19042935 <630455664@qq.com>
2019-10-16 18:11:23 +0800

    帮助中心完成

--- pages/me/help/help.js
+++ pages/me/help/help.js
@@ -5,62 +5,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    "problems": [{
+      "tittle": "如何找到斌斌回收机器？",
+      "content": "打开微信小程序查找",
+      "isShow": false
+    }, {
+      "tittle": "上面回收免费吗？",
+      "content": "我们不收取费用",
+      "isShow": false
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  //常见问题item点击
+  onProblemTittleClick: function(event) {
+    var index = event.currentTarget.dataset.index;
+
+    var current = this.data.problems[index];
+    current.isShow = !current.isShow;
+    var newProblems = this.data.problems;
+    newProblems[index] = current;
+    this.setData({
+      problems: newProblems
+    });
   }
 })
