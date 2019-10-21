704826dcbf2916ec38a5a290c0b95dada43cd65e
19042935 <630455664@qq.com>
2019-10-21 14:14:38 +0800

    积分明细完成

--- pages/me/myIntegral/myIntegral.js
+++ pages/me/myIntegral/myIntegral.js
@@ -5,62 +5,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    "integralList": [{
+      "type": "回收成功",
+      "integral": "+5"
+    }, {
+        "type": "回收成功",
+        "integral": "+5"
+      }, {
+        "type": "回收成功",
+        "integral": "+5"
+      }, {
+        "type": "积分商城消费",
+        "integral": "-10"
+      }, {
+        "type": "签到",
+        "integral": "+5"
+      }, {
+        "type": "回收成功",
+        "integral": "+5"
+      }]
+    ,
+    "currentIndex":0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  get:function(){
+    
   }
 })
