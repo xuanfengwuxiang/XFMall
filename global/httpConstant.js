6483cec54223a5b3b7fb47ba97c6ae6677605c79
19042935 <630455664@qq.com>
2019-11-30 13:07:56 +0800

    积分详情接口完成

--- global/httpConstant.js
+++ global/httpConstant.js
@@ -3,7 +3,10 @@
 var REGISTER = '/me/register'; //注册
 
 var LOGOUT = '/me/logout'; //退出登录
+
+var INTEGRAL_DETAIL = '/me/getIntegralDetail';//积分详情
 export {
   REGISTER,
-  LOGOUT
+  LOGOUT,
+  INTEGRAL_DETAIL
 }
--- pages/me/myIntegral/myIntegral.js
+++ pages/me/myIntegral/myIntegral.js
@@ -1,4 +1,9 @@
 // pages/me/myIntegral/myIntegral.js
+const app = getApp()
+import {
+  INTEGRAL_DETAIL
+} from '../../../global/httpConstant.js'
+
 Page({
 
   /**
@@ -52,7 +57,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.getIntegral();
   },
 
   /**
@@ -92,5 +97,45 @@ Page({
 
   get: function() {
 
+  },
+
+  //获取我的积分
+  getIntegral() {
+
+    if (app.globalData.loginBean == null) {
+      this.showErrorToast('登录后查看积分详情！');
+      return;
+    }
+
+    wx.request({
+
+      url: app.globalData.host + INTEGRAL_DETAIL,
+      data: {
+        tel: app.globalData.loginBean.tel
+      },
+      success: res => {
+        console.log(res.data);
+
+        if (res.data.isOK === false) {
+          this.showErrorToast('积分详情获取失败');
+          return
+        };
+
+        this.setData({
+          integralList: res.data.data
+        });
+      },
+
+      fail: err => {
+        console.log(err);
+      }
+    })
+  },
+
+  showErrorToast(value) {
+    wx.showToast({
+      title: '' + value,
+      image: '/images/global/ic_toast_error.png'
+    })
   }
 })
