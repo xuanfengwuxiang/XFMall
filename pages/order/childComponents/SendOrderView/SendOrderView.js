14e56e68226679cf414dbe6469c2c6e677273eeb
19042935 <630455664@qq.com>
2020-05-24 23:11:59 +0800

    订单界面部分完成

--- pages/order/childComponents/SendOrderView/SendOrderView.js
+++ pages/order/childComponents/SendOrderView/SendOrderView.js
@@ -14,7 +14,23 @@ Component({
    * 组件的初始数据
    */
   data: {
-    "tittles": ["未处理", "处理中", "已送达", "所有"]
+    "tittles": ["未处理", "处理中", "已送达", "所有"],
+    "sendOrders": [{
+      "orderId": "2020052400001",
+      "products": [{
+        "cmName": "苹果",
+        "cmNum": "3",
+        "pic": "http://a3.att.hudong.com/14/75/01300000164186121366756803686.jpg"
+      }, {
+        "cmName": "苹果",
+        "cmNum": "3",
+        "pic": "http://a3.att.hudong.com/14/75/01300000164186121366756803686.jpg"
+      }, {
+        "cmName": "苹果",
+        "cmNum": "3",
+        "pic": "http://a3.att.hudong.com/14/75/01300000164186121366756803686.jpg"
+      }]
+    }]
   },
 
   /**
